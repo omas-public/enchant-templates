ffd675f22cd0812f6470ce2bdcc33d816cc4a8e4
omas-public <omas@std.it-college.ac.jp>
2015-11-06 18:37:30 +0900

    some fix

--- template/normal/main.js
+++ template/normal/main.js
@@ -13,70 +13,72 @@ var gs = (enchant.Class.create({
 }))();
 
 // 拡張Core
-var Core = enchant.Class.create(enchant.nineleap.Core, {
+var MyCore = enchant.Class.create(enchant.nineleap.Core, {
   initialize: function(background) {
-    enchant.nineleap.Core.call(this); // Coreを継承
-    this.fps = gs.fps; // fpsをセット
-    this.width = gs.width; // 画面の幅
-    this.height = gs.height; // 画面の高さ
-    this.setStage(background); // 背景のセット(色)
-    this.loadAssets(gs.assets); // アセットの読み込み
+    enchant.nineleap.Core.call(this);   // Coreを継承
+    this.fps = gs.fps;                  // fpsをセット
+    this.width = gs.width;              // 画面の幅
+    this.height = gs.height;            // 画面の高さ
+    this.setStage(background);          // 背景のセット(色)
+    this.loadAssets(gs.assets);         // アセットの読み込み
+  },
+
+  setStage: function(background) {
+    stage = this.rootScene;
+    stage.backgroundColor = background || 'white';
+  },
+
+  // アセットのパスを配列で返す
+  loadAssets: function(assets) {
+    var keyname = 'path';
+    var assetsPathList = [];
+
+    for (var obj in assets) {
+      if (assets[obj].hasOwnProperty(keyname)) {
+        assetsPathList.push(assets[obj][keyname]);
+      }
+    }
+    if (assetsPathList !== 0) {
+      this.preload(assetsPathList);
+    }
   }
-
-  , setStage: function(background) {
-      stage = this.rootScene;
-      stage.backgroundColor = background || 'white';
-    }
-    // アセットのパスを配列で返す
-  , loadAssets: function(assets) {
-      var keyname        = 'path';
-      var assetsPathList = [];
-
-      for (var obj in assets) {
-        if (assets[obj].hasOwnProperty(keyname)) {
-          assetsPathList.push(assets[obj][keyname]);
-        }
-      }
-      if (assetsPathList !== 0) {
-        this.preload(assetsPathList);
-      }
-    }
 });
 
 
-//	==================================================
-//	Template create 2015-10-30
-//	==================================================
+//  ==================================================
+//  Template create 2015-10-30
+//  ==================================================
 
 /*
 * 画像を使用したい場合
 
 gs.assets.${name} = {
-  path   : './assets/${filename}',
-  height : ${height}
-  , width: ${width}
-  , frame: [${frame}]
+  path     : './assets/${filename}'
+  , height : ${height}
+  , width  : ${width}
+  , frame  : [${frame}]
 };
 
 var ${spritename} = new Sprite(
   gs.assets.${name}.width, gs.assets.${name}.height);
 ${spritename}.image = game.assets[gs.assets.${name}.path];
 ${spritename}.frame = game.assets[gs.assets.${name}.frame];
+
 */
 
 
 window.onload = function() {
-  game = new Core('mintcream');
+  game = new MyCore('mintcream');
   game.onload = function() {
 
-// ==================================================================
+    // ==================================================================
 
     stage.addChild(new Label('template for enchant js'));
     stage.on('touchend', function() {
       game.end();
     });
 
-// ==================================================================
+    // ==================================================================
   };
   game.start();
 };
--- template/simple/main.js
+++ template/simple/main.js
@@ -3,24 +3,28 @@ enchant();
 
 var game, stage;
 var gs = {
-  height : 320
-  , width: 320
-  , fps: 15
+  height: 320,
+  width: 320,
+  fps: 15
 };
 
 window.onload = function() {
-  game  = new Core(gs.width, gs.height);
+
+  game = new Core(gs.width, gs.height);
   // game.preload('./assets/filename.png'); //画像を使う場合
   stage = game.rootScene;
   stage.backgroundColor = 'yellow';
+
   game.onload = function() {
-//  ==================================================
+
+    // Code Write Here ===================================
 
 
-//  ==================================================
+
+
+    //  ==================================================
+
   };
+
   game.start();
 };
-
-
-
